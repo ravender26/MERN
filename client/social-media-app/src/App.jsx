c9d4d5d662a1a996bfb6aecc56331fc21dd6d933
ravender26 <ravender.jadon123@gmail.com>
2023-06-18 01:13:37 +0530

    povite mui theme

--- client/social-media-app/src/App.jsx
+++ client/social-media-app/src/App.jsx
@@ -7,7 +7,11 @@ import { useDispatch } from "react-redux"
 import { useEffect } from 'react';
 import { getPosts } from "./redux/actions/posts";
 
+import { createTheme } from '@mui/material';
+import { ThemeProvider } from '@mui/styles';
+
 function App() {
+  const theme = createTheme()
   const classes = useStyles();
   const dispatch = useDispatch();
 
@@ -16,25 +20,27 @@ function App() {
   }, [dispatch]);
 
   return (
-    <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form />
-            </Grid>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+          <img className={classes.image} src={memories} alt="icon" height="60" />
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
 
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
+    </ThemeProvider>
   )
 }
 
