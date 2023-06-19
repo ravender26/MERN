c10c6c1e7b1ec08123e5779446687eb3f93c26cf
ravender26 <ravender.jadon123@gmail.com>
2023-06-20 00:27:53 +0530

    feat: update design

--- client/social-media-app/src/App.jsx
+++ client/social-media-app/src/App.jsx
@@ -32,7 +32,9 @@ function App() {
         </AppBar>
         <Grow in>
           <Container>
-            <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid
+              // className={classes.mainContainer}
+              container justify="space-between" alignItems="stretch" spacing={3}>
               <Grid item xs={12} sm={7}>
                 <Posts setCurrentId={setCurrentId} />
               </Grid>
