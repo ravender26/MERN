ba1a1d1fae27289cac2dbf322b8e4ad075b560a1
ravender26 <ravender.jadon123@gmail.com>
2023-06-20 23:20:09 +0530

    feat: add google auth

--- client/social-media-app/src/App.jsx
+++ client/social-media-app/src/App.jsx
@@ -3,17 +3,27 @@ import { createTheme } from '@mui/material';
 import { ThemeProvider } from '@mui/styles';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+import { Route, Routes } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
   const theme = createTheme()
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container maxWidth="lg">
-        <Navbar />
-        <Home />
-      </Container>
-    </ThemeProvider>
+    <>
+      <GoogleOAuthProvider clientId='667738762491-sdb2be6lr5n5dufbvab40bfrdrbddhb0.apps.googleusercontent.com'>
+        <ThemeProvider theme={theme}>
+          <Container maxWidth="lg">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="auth" element={<Auth />} />
+            </Routes>
+          </Container>
+        </ThemeProvider >
+      </GoogleOAuthProvider>
+    </>
   )
 }
 
